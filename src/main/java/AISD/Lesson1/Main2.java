83eb08689b6187da184401a7bbc6924cb847699a
VlaADanilov <helloampro@gmail.com>
2024-02-21 11:24:06 +0300

    This is my homeTask

--- src/main/java/AISD/Lesson1/Main2.java
+++ src/main/java/AISD/Lesson1/Main2.java
@@ -2,18 +2,31 @@ package AISD.Lesson1;
 
 import java.util.Arrays;
 
+// n - длина строки
+// p - максимальное кол-во вхождения среди элементов
+// q - кол-во разных элементов
+
 public class Main2 {
     public static void main(String[] args) {
-        String str = "Hello, world!";
+        String str = "@@@#asdfghYYTRRRRRRRFFVVBNJSYUEIWO(*(*(*88*(5/+```";
         paintFigure(str);
+        // результат по времени: O(n*log(n) + pq) ==> O(n*log(n))
+        // результат по памяти: O(n + q) ==> O(n)
     }
+
 
     public static void paintFigure(String str){
         String result = createStr(str);
+        // O(n) - память
+        // O(n log(n)) время
         int[] arr = createChars(str, result);
+        // O(q) - память
+        //O(n log(p)) - время
         for (int i = maxArr(arr) - 1; i >= 0; i--){
+            // O(p) - время
             String pr = "";
             for (int x = 0; x < result.length(); x++){
+                // O(q) - время
                 pr += arr[x] - 1 >= i   ? "#" : " ";
             }
             System.out.println(pr);
@@ -31,42 +44,54 @@ public class Main2 {
 
     private static int[] createChars(String str, String result){
         int[] ret = new int[result.length()];
-        for (int i = 0; i < str.length(); i++){
-            if(str.charAt(i) != ' ' && str.charAt(i) != '\n') ret[indexOfChar(result, str.charAt(i))]++;
+        for (int i = 0; i < str.length(); i++){ // O(n)
+            if(str.charAt(i) != ' ' && str.charAt(i) != '\n') ret[indexOfChar(result, str.charAt(i))]++; //O(log(p))
         }
         return ret;
     }
 
-    private static int indexOfChar(String str, char p){
-        for (int i = 0; i < str.length(); i++){
-            if (str.charAt(i) == p) return i;
+    private static int binaryFing(String str, int l, int r, char chr){
+        //O(log p) время
+
+        if (l == r){
+            return l;
         }
-        return -1;
+        int m = l + (r - l) / 2;
+        if (str.charAt(m) > chr){
+            return binaryFing(str,l, m - 1, chr);
+        }
+        else{
+            if (str.charAt(m) < chr) return binaryFing(str, m + 1, r, chr);
+        }
+        return m;
+    }
+
+    private static int indexOfChar(String str, char p){
+        // O(log n) время
+        return binaryFing(str, 0, str.length() - 1, p);
     }
 
     private static String createStr(String str){
         String ret = "";
-        for(int i = 0; i < str.length(); i++){
-            if (!inStr(ret, str.charAt(i))){
-                if(str.charAt(i) != ' ' && str.charAt(i) != '\n') ret += str.charAt(i);
-            }
-        }
-        char[] chars = new char[ret.length()];
-        for (int i = 0; i < ret.length(); i++){
-            chars[i] = ret.charAt(i);
+        char[] chars = new char[str.length()];
+        for (int i = 0; i < str.length(); i++){
+            // O(n) время
+            chars[i] = str.charAt(i);
         }
         Arrays.sort(chars);
-        ret = "";
-        for (int i = 0; i < chars.length; i++){
-            ret += chars[i];
+        //O(n log(n)) время
+        for(int i = 0; i < chars.length; i++){
+            //O(n) время
+            if (!inStr(ret, chars[i])){
+                //O(1) время
+                if(chars[i] != ' ' && chars[i] != '\n') ret += chars[i];
+            }
         }
         return ret;
     }
 
     private static boolean inStr(String str, char p){
-        for (int i = 0; i < str.length(); i++){
-            if (str.charAt(i) == p) return true;
-        }
-        return false;
+        if (str.length() == 0) return false;
+        return str.charAt(str.length() - 1) == p;
     }
 }

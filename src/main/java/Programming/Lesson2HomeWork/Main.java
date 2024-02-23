2b8b280d3823349915396922b59c5c28bcd0096a
VlaADanilov <helloampro@gmail.com>
2024-02-23 12:31:31 +0300

    This is my homeTask

---
+++ src/main/java/Programming/Lesson2HomeWork/Main.java
@@ -0,0 +1,24 @@
+package Programming.Lesson2HomeWork;
+
+import Programming.Lesson1.EmptyElementException;
+
+public class Main {
+    public static void main(String[] args) throws EmptyElementException {
+        OrderedList<Integer> ord = new OrderedList<>();
+        for (int i = 0; i < 10; i++) {
+            ord.add(Integer.valueOf(i));
+        }
+        ord.add(5);
+        ord.add(5);
+        ord.add(5);
+        ord.add(5);
+        ord.add(5);
+        ord.add(5);
+        ord.add(8);
+        ord.add(9999);
+        ord.deleteAll(5);
+        ord.add(Integer.valueOf(99));
+        System.out.println(ord);
+        System.out.println(ord.getIndex(Integer.valueOf(5)));
+    }
+}
---
+++ src/main/java/Programming/Lesson2HomeWork/OrderedList.java
@@ -0,0 +1,89 @@
+package Programming.Lesson2HomeWork;
+
+import Programming.Lesson1.EmptyElementException;
+import Programming.Lesson1.LinkedList.Element;
+import Programming.Lesson1.LinkedList.LinkedList;
+
+import java.util.Arrays;
+
+public class OrderedList<T extends Comparable> extends LinkedList {
+    public OrderedList(){}
+
+    public OrderedList(Element e){
+        this.head = head;
+        len++;
+    }
+
+    public OrderedList(T[] arr){
+        this();
+        Arrays.sort(arr);
+        len = arr.length;
+        if (arr.length != 0){
+            head = new Element(arr[0]);
+            Element temp = head;
+            for (int i = 1; i < arr.length; i++){
+                temp.next = new Element(arr[i]);
+                temp = temp.next;
+            }
+        }
+
+    }
+
+    public void add(T e) throws EmptyElementException{
+        if (e == null){
+            throw new EmptyElementException();
+        }
+
+        if (head == null){
+            head = new Element(e);
+        }
+        else{
+            Element temp = head;
+            if (e.compareTo(head.value) == -1){
+                head = new Element(e);
+                head.next = temp;
+            }
+            Element prev = head;
+            do{
+                prev = temp;
+                temp = temp.next;
+            }while (temp != null && e.compareTo(temp.value) == 1);
+
+            Element now = new Element(e);
+            prev.next = now;
+            now.next = temp;
+        }
+        len++;
+    }
+
+    public int getIndex(T e){
+        Element[] elems = new Element[size()];
+        Element temp = head;
+        for (int i = 0; i < size(); i++){
+            elems[i] = temp;
+            temp = temp.next;
+        }
+        int index = binaryFing(elems,0,size() - 1, e);
+        while(index != 0){
+            if (!elems[index - 1].equals(elems[index])) break;
+            index--;
+        }
+        return index;
+    }
+
+    private int binaryFing(Element[] elems ,int l, int r, T e){
+        //O(log n) время
+
+        if (l == r){
+            return l;
+        }
+        int m = l + (r - l) / 2;
+        if (e.compareTo(elems[m].value) == -1){
+            return binaryFing(elems,l, m - 1, e);
+        }
+        else{
+            if (e.compareTo(elems[m].value) == 1) return binaryFing(elems, m + 1, r, e);
+        }
+        return m;
+    }
+}
